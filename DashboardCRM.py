db7590819370274a92aa87c461cdbf10249ad5f8
Diana Mendizabal <artedeamorperu@gmail.com>
2025-06-09 02:09:50 -0500

    Seguimiento Mensajes

--- DashboardCRM.py
+++ DashboardCRM.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+from streamlit_option_menu import option_menu
+import webbrowser as webbrowser
+import time, webbrowser, pyautogui
+from datetime import datetime
+import pywhatkit
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def Dashboard():
     st.markdown("DASHBOARDDD")
+
+    conn = st.connection("gsheets", type=GSheetsConnection)
+
+    existing_data = conn.read(worksheet="SEGUIMIENTO",usecols=list(range(7)),ttl=7)
+    mensaje_data = conn.read(worksheet="MENSAJES",usecols=list(range(3)),ttl=3)
+
+    clientes_antiguos = conn.read(worksheet="MENSAJE_DIARIO",usecols=list(range(6)),ttl=6)
+
+    fig, ax = plt.subplots()
+    ax.bar([1, 2, 3], [3, 2, 1])
+    st.pyplot(fig)
--- principal.py
+++ principal.py
@@ -6,21 +6,38 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import requests
 from streamlit_option_menu import option_menu
+from datetime import datetime #PARA IMPORTA DEL DÍA FECHA
+
 import Seguimiento #IMPORTANDO EL SEGUIMIENTO.PY PARA USAR SUS FUNCIONES
 import DashboardCRM #IMPORTANDO LA HOJA DE SAHBOARD CRM PARA USAR SUS FUNCIONES
+import Dashboard_General
 import os
 
 
 datos_map = pd.read_json("map.json")
 
 
+
 def main ():
 
     with st.sidebar:
+
+        col1, col2, col3 = st.columns([1, 2, 1])
+    
+        with col2:
+
+            st.image('pages/img/logoh.jpg',width=200)
+
         selected = option_menu(
             menu_title = "Menú Principal",
-            options = ["Cliente potencial","Seguimiento","Dashboard CRM"],
+            options = ["Cliente potencial","Seguimiento","Dashboard CRM","Dashboard General"],
         )
+
+        col4, col5, col6 = st.columns([1, 2, 1])
+
+        with col5:
+
+            st.markdown("<marquee> **By Diana Mendizabal** </marquee>", unsafe_allow_html=True)
 
     if selected == "Seguimiento":
         Seguimiento.seguirCliente()
@@ -28,14 +45,15 @@ def main ():
         PrincipalP()
     elif selected == "Dashboard CRM":
         DashboardCRM.Dashboard()
+    elif selected == "Dashboard General":
+        Dashboard_General.Dashboard_General()
 
 
 #ESTE CODIGO SERÁ DE LA PÁGINA PRINCIPAL QUE ES DE AÑADIR CLIENTE POTENCIAL
 
 def PrincipalP ():
     # url = "https://docs.google.com/spreadsheets/d/1USL95W4CYBvGOIpvBLlcO-3djRTRjJer2DbkCl48vpQ"
-    st.title ("APLICACIÓN PACEÑA")
-    st.write("## Crear nuevo cliente potencial")
+    st.title ("AGREGAR O EDITAR CLIENTES")
     conn = st.connection("gsheets", type=GSheetsConnection)
 
 
@@ -54,8 +72,8 @@ def PrincipalP ():
     existing_data = existing_data.dropna(how="all")
 
 
-
-    st.dataframe(existing_data)
+    ##PAR QUE SE MUESTRE LOS 5 PRIMEROS
+    st.dataframe(existing_data.head(5))
 
 
 
@@ -108,7 +126,8 @@ def PrincipalP ():
 
     existing_data["TelefonoI"] = existing_data["TelefonoI"].apply(lambda x: str(int(float(x))) if pd.notnull(x) else "")
 
-
+    
+    existing_data["ID"] = existing_data["ID"].apply(lambda x: str(int(float(x))) if pd.notnull(x) else "")
 
 
     #PARA AÑADIR COLOR AL BOTON DE SELECCIONAR TELEFONO
@@ -140,7 +159,7 @@ def PrincipalP ():
 
         with col2:
             st.markdown("""
-            <div style='background-color:#ff4d4d; padding: 5px; border-radius: 10px;'>
+            <div style='background-color:#ff4d4d; padding: 5px; border-radius: 10px; text-align: center'>
                 <label style='color:white; font-weight:bold; font-size:18px; '>EDITAR LEAD POR TELÉFONO</label>
             """, unsafe_allow_html=True)
 
@@ -152,9 +171,21 @@ def PrincipalP ():
 
     #cliente_data = None
 
+    #st.write("## Crear nuevo cliente potencial")
+
+    st.markdown("""
+        <div style='background-color:#f7b260; padding: 5px; border-radius: 10px; text-align: center'>
+        <label style='color:black; font-weight:bold; font-size:18px; '>AGREGAR NUEVO CLIENTE</label>
+        """, unsafe_allow_html=True)
+
+    
+
 
     if cliente_ids: # and "form_initialized" not in st.session_state:
         cliente_data = existing_data[existing_data["TelefonoI"] == cliente_ids].iloc[0]
+
+
+
 
            # idI.value = cliente_data["ID"]
            # "Nombre": Nombre,
@@ -177,7 +208,8 @@ def PrincipalP ():
         st.session_state.Estado = cliente_data["Estado"]
         st.session_state.Comentario = cliente_data["Comentario"] if pd.notna(cliente_data["Comentario"]) else ""
 
-
+    fecha_str = datetime.now().date() ##PARA TENER LA FECHA ACTUAL EN CASO NO SE COLOQUE EN EL INPUT DE FECHA LEAD
+    
     #FORMULARIO
 
     with st.form(key="CRM_form"):
@@ -190,7 +222,7 @@ def PrincipalP ():
         Sexo = st.selectbox("Sexo", options = SEXO_i, index=SEXO_i.index(st.session_state["Sexo"]) if "Sexo" in st.session_state and st.session_state["Sexo"] in SEXO_i else None)
         Departamento = st.selectbox("Departamento", options = datos_map.keys(), index=list(datos_map.keys()).index(st.session_state["Departamento"]) if "Departamento" in st.session_state else 0)
         Provincia = st.selectbox("Provincia", options = list(datos_map[Departamento].keys()), index=list(datos_map[Departamento].keys()).index(st.session_state["Provincia"]) if "Provincia" in st.session_state else 0)
-        Distrito = st.selectbox("Distrito", options = list(datos_map[Departamento][Provincia].keys()), index=list(datos_map[Departamento][Provincia].keys()).index(st.session_state["Distrito"]) if "Distrito" in st.session_state and pd.notna(cliente_data["Distrito"]) and cliente_data["Departamento"] != "" else None)
+        Distrito = st.selectbox("Distrito", options = list(datos_map[Departamento][Provincia]), index=list(datos_map[Departamento][Provincia].keys()).index(st.session_state["Distrito"]) if "Distrito" in st.session_state and pd.notna(cliente_data["Distrito"]) and cliente_data["Departamento"] != "" else 0)
         Telefono = st.text_input("Teléfono*", value=st.session_state.get("Telefono", ""), max_chars=11)
         Tipo = st.multiselect("Tipo producto*", options = TIPO_i, default=st.session_state.get("Tipo", []))
         Ocasion = st.selectbox("Ocasión compra*", options = OCASION_i, index=OCASION_i.index(st.session_state["Ocasion"]) if "Ocasion" in st.session_state and st.session_state["Ocasion"] in OCASION_i else None)
@@ -208,19 +240,40 @@ def PrincipalP ():
         existing_data["TelefonoI"] = existing_data["TelefonoI"].astype(str) #PARA CONVERTIR EL EXISTING DATA TELEFONO, PARA CAMBIAR ESA PARTE DEL DATAFRAME SU TIPO
         
         if submit_button:
-            if not Tipo or not  Ocasion or not Fecha_Lead or not Medio_Adqui or not Estado or not Telefono:
+            if not Tipo or not  Ocasion  or not Medio_Adqui or not Estado or not Telefono:
                 st.warning("Falta llenar los campos obligatorios")
                 st.stop()
             elif existing_data["TelefonoI"].str.contains(Telefono).any():
-                st.warning("El cliente ya existe")
-                st.stop()
-
+               ## st.warning("El cliente ya existe")
+               ## st.stop()
+                
+                if existing_data["TelefonoI"].str.contains(Telefono).any():
+                    st.session_state.cliente_existente = True
+                    st.session_state.datos_temporales = {
+                        "ID": idI,
+                        "Nombre": Nombre,
+                        "Sexo": Sexo,
+                        "Departamento": Departamento,
+                        "Provincia": Provincia,
+                        "Distrito": Distrito,
+                        "TelefonoI": Telefono,
+                        "Tipo": ", ".join(Tipo),
+                        "Ocasión": Ocasion,
+                        "MedioAdquisicion": Medio_Adqui,
+                        "Campaña": Campaña,
+                        "FechaLead": fecha_str.strftime("%Y-%m-%d") if Fecha_Lead is None else Fecha_Lead.strftime("%Y-%m-%d"),
+                        "FechaUltimoContacto": fecha_str.strftime("%Y-%m-%d") if Fecha_Lead is None else Fecha_Lead.strftime("%Y-%m-%d") if Fecha_UC is None  else Fecha_UC.strftime("%Y-%m-%d"),
+                            # O un valor por defecto como "" )
+                        ##ESO ES PARA QUE SI NO HAYA FECHA ULTIMO CONTACTO SE AGREGUE LA FECHALEAD PARA QUE NO HAYA ERROR
+                        "Estado": Estado,
+                        "Comentario": Comentario
+                    }
             else:
 
                 cliente_potencial_new = pd.DataFrame (
                     [
                         {
-                            "ID": idI,
+                            "ID": int(float(existing_data["ID"].max()))+1,
                             "Nombre": Nombre,
                             "Sexo": Sexo,
                             "Departamento": Departamento,
@@ -231,8 +284,8 @@ def PrincipalP ():
                             "Ocasión": Ocasion,
                             "MedioAdquisicion": Medio_Adqui,
                             "Campaña": Campaña,
-                            "FechaLead": Fecha_Lead.strftime("%Y-%m-%d"),
-                            "FechaUltimoContacto": Fecha_UC.strftime("%Y-%m-%d"),
+                            "FechaLead": fecha_str.strftime("%Y-%m-%d") if Fecha_Lead is None else Fecha_Lead.strftime("%Y-%m-%d"),
+                            "FechaUltimoContacto": fecha_str.strftime("%Y-%m-%d") if Fecha_Lead is None else Fecha_Lead.strftime("%Y-%m-%d") if Fecha_UC is None  else Fecha_UC.strftime("%Y-%m-%d"),
                             "Estado": Estado,
                         }
                     ]
@@ -243,6 +296,33 @@ def PrincipalP ():
                 conn.update(worksheet="CRM",data=update_df)
 
                 st.success("Cliente potencial añadido")
+
+
+    if st.session_state.get("cliente_existente", False):
+
+        st.session_state.mostrar_popup = True
+
+        if st.session_state.mostrar_popup:
+            
+            st.markdown("EL CONTACTO YA EXISTE ¿Quieres cambiarlo?");
+            col1, col2 = st.columns([1, 1])
+            with col1:
+                if st.button("✅ Sí, reemplazar"):
+                    st.success("Cliente reemplazado.")
+                    index = existing_data[existing_data["TelefonoI"] == st.session_state.datos_temporales["TelefonoI"]].index[0]
+                    for key, value in st.session_state.datos_temporales.items():
+                        existing_data.at[index, key] = value
+
+                    # Guardar cambios en la hoja de cálculo
+                    conn.update(worksheet="CRM", data=existing_data)
+
+                    # Limpiar estados
+                    st.session_state.mostrar_popup = False
+                    st.session_state.cliente_existente = False
+                    st.session_state.datos_temporales = {}
+            with col2:
+                if st.button("❌ Cancelar"):
+                    st.session_state.mostrar_popup = False
 
 
     #def click_function():
